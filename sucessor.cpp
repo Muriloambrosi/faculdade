7b30f7b8edc8507a67c76d1c0748d4d281e55c1a
Muriloambrosi <144922299+Muriloambrosi@users.noreply.github.com>
2026-08-04 22:03:25 -0300

    Add files via upload

---
+++ sucessor.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int numero;
+int soma;
+	
+main(){
+   
+     printf("digite um numero");
+     scanf("%d", &numero);
+     soma = numero + 1;
+     printf("%i", soma);
+	 
+	 }
+
