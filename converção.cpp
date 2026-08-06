56363a7b9fccd9fca1371c5f9c22db2335787111
Muriloambrosi <144922299+Muriloambrosi@users.noreply.github.com>
2026-08-06 19:48:31 -0300

    Add files via upload

---
+++ converção.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+float dolar;
+float real;
+
+main(){
+	
+	printf("digite o valor em dolares\n");
+	scanf("%f", &dolar);
+	
+	real = dolar * 5.12;
+	
+	printf("o seu valor em real e %f", real);
+	
+
+	
+}
