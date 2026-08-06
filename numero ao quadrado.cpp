754c206b5e7beed1f141b5d2156127e9d0ebbe83
Muriloambrosi <144922299+Muriloambrosi@users.noreply.github.com>
2026-08-06 20:29:45 -0300

    Add files via upload

---
+++ numero ao quadrado.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+	
+	int numero;
+	int quadrado;
+	
+	main(){
+	
+	printf("digite um numero\n");
+	scanf("%i", &numero);
+	
+	quadrado = numero * numero;
+	
+	printf("\nquadrado: %i", quadrado);
+	}
